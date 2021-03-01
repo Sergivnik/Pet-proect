4ce48c9c85fc3bd1fc99446d578fb0aa3c4f168a
Sergivnik <65278578+Sergivnik@users.noreply.github.com>
2021-03-01 15:59:39 +0300

    v-1.22

--- API/server.js
+++ API/server.js
@@ -9,4 +9,23 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(router);
 
+// const mysql = require("mysql2");
+// const options = require("./models/config.js");
+
+// var XLSX = require("xlsx");
+// var workbook = XLSX.readFile("./DB/Колдовство.xlsb");
+// var sheet_name_list = workbook.SheetNames;
+// var xlData = XLSX.utils.sheet_to_json(workbook.Sheets[sheet_name_list[1]]);
+// const db = mysql.createConnection(options).promise();
+// xlData.forEach(async (elem) => {
+//   try {
+//     let [data] = await db.query("INSERT INTO drivers SET ?", {
+//       id: null,
+//       value: elem.ФИО,
+//     });
+//   } catch (err) {
+//     callback({ error: err });
+//   }
+// });
+
 app.listen(3000, () => console.log("Example app listening on port 3000!"));
