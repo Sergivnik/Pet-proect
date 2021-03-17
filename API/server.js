01d27c6936508228b660247f146c45e971c772ac
Serg <sergivnik@mail.ru>
2021-03-17 06:14:11 +0300

    v-1.42

--- API/server.js
+++ API/server.js
@@ -3,7 +3,8 @@ const path = require("path");
 const router = require("./routers");
 
 const app = express();
-app.use(express.static(path.join(__dirname, "../dist")));
+app.use(express.static(path.join(__dirname, "./public")));
+//app.use(express.static(path.join(__dirname, "../dist")));
 app.use(express.static(path.join(__dirname, "documents")));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
