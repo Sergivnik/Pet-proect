df44c3f82daae8c92505f8d41919ccc533f53a26
Serg <sergivnik@mail.ru>
2021-04-14 06:17:17 +0300

    v-2.19

--- src/components/filterList/filterList.jsx
+++ src/components/filterList/filterList.jsx
@@ -18,44 +18,70 @@ export const FilterList = (props) => {
     let [...arr] = chosenList;
     console.log(e.target.checked);
     let check = arr.findIndex((elem) => elem.id === e.target.value);
-    if (check>=0) {
+    if (check >= 0) {
       arr.splice(check, 1);
     } else {
       arr.push({ id: e.target.value, value: e.target.name });
     }
     setChosenList(arr);
   };
+  const handleClickP = (e) => {
+    if (e.target.localName == "p") {
+      e.target.children[0].checked = !e.target.children[0].checked;
+      let [...arr] = chosenList;
+      let check = arr.findIndex(
+        (elem) => elem.id === e.target.children[0].value
+      );
+      if (check >= 0) {
+        arr.splice(check, 1);
+      } else {
+        arr.push({
+          id: e.target.children[0].value,
+          value: e.target.children[0].name,
+        });
+      }
+      setChosenList(arr);
+    }
+  };
 
   return (
     <div className="filterDiv">
-      {chosenList.length > 0 &&
-        chosenList.map((elem, index) => {
+      <div className="filterDivChosenP">
+        {chosenList.length > 0 &&
+          chosenList.map((elem, index) => {
+            return (
+              <p key={`chosenP-${index}`} className="filterPChosen">
+                {elem.value}
+              </p>
+            );
+          })}{" "}
+      </div>
+      <input type="text" value={text} onChange={changeList} />
+      <div className="filterDivList">
+        {list.map((elem) => {
+          let chosenElem = chosenList.find((item) => item.id == elem._id);
+          let checked = "";
+          if (chosenElem) checked = "checked";
           return (
-            <p key={`chosenP-${index}`} className="filterPChosen">
-              {elem.value}
-            </p>
+            <div key={elem._id}>
+              <p className="filterP" onClick={handleClickP}>
+                <input
+                  type="checkbox"
+                  value={elem._id}
+                  name={elem.value}
+                  checked={checked}
+                  onChange={choiseValue}
+                />
+                {elem.value}
+              </p>
+            </div>
           );
         })}
-      <input type="text" value={text} onChange={changeList} />
-      {list.map((elem) => {
-        let chosenElem = chosenList.find((item) => item.id == elem._id);
-        let checked = "";
-        if (chosenElem) checked = "checked";
-        return (
-          <div key={elem._id}>
-            <p className="filterP">
-              <input
-                type="checkbox"
-                value={elem._id}
-                name={elem.value}
-                checked={checked}
-                onChange={choiseValue}
-              />
-              {elem.value}
-            </p>
-          </div>
-        );
-      })}
+      </div>
+      <div className="filterDivBtn">
+        <button>Ok</button>
+        <button>Clear</button>
+      </div>
     </div>
   );
 };
--- src/components/oders/userThead.jsx
+++ src/components/oders/userThead.jsx
@@ -5,13 +5,15 @@ import "./oders.sass";
 
 export const UserThead = (props) => {
   const [showFilter, setShowFilter] = useState(false);
+  const [colNumber, setColNumber] = useState(null);
 
   const driversList = useSelector((state) => state.oderReducer.driverlist);
   const clientList = useSelector((state) => state.oderReducer.clientList);
   const citieslist = useSelector((state) => state.oderReducer.citieslist);
 
-  const handleClickFilter = () => {
+  const handleClickFilter = (e) => {
     setShowFilter(true);
+    setColNumber(e.target.parentElement.parentElement.cellIndex);
   };
   return (
     <thead>
@@ -27,11 +29,52 @@ export const UserThead = (props) => {
               alt=""
             />
           </button>
-          {showFilter && <FilterList name="Driver" arrlist={driversList} />}
+          {showFilter && colNumber === 1 && (
+            <FilterList name="Driver" arrlist={driversList} />
+          )}
         </td>
-        <td className="odersTd odersTRheader">Заказчик</td>
-        <td className="odersTd odersTRheader">Загрузка</td>
-        <td className="odersTd odersTRheader">Выгрузка</td>
+        <td className="odersTd odersTRheader">
+          Заказчик
+          <button className="theadBtnFilter" onClick={handleClickFilter}>
+            <img
+              height="20"
+              weight="20"
+              src="http://localhost:3000/img/down-arrow.png"
+              alt=""
+            />
+          </button>
+          {showFilter && colNumber === 2 && (
+            <FilterList name="Driver" arrlist={clientList} />
+          )}
+        </td>
+        <td className="odersTd odersTRheader">
+          Загрузка
+          <button className="theadBtnFilter" onClick={handleClickFilter}>
+            <img
+              height="20"
+              weight="20"
+              src="http://localhost:3000/img/down-arrow.png"
+              alt=""
+            />
+          </button>
+          {showFilter && colNumber === 3 && (
+            <FilterList name="Driver" arrlist={citieslist} />
+          )}
+        </td>
+        <td className="odersTd odersTRheader">
+          Выгрузка
+          <button className="theadBtnFilter" onClick={handleClickFilter}>
+            <img
+              height="20"
+              weight="20"
+              src="http://localhost:3000/img/down-arrow.png"
+              alt=""
+            />
+          </button>
+          {showFilter && colNumber === 4 && (
+            <FilterList name="Driver" arrlist={citieslist} />
+          )}
+        </td>
         <td className="odersTd odersTRheader">Цена клиента</td>
         <td className="odersTd odersTRheader">Цена водителя</td>
         <td className="odersTd odersTRheader">Доверенность</td>
