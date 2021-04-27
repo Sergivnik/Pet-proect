fe9640cc849acc0bdf7d872977891b5fb0e58064
Sergivnik <65278578+Sergivnik@users.noreply.github.com>
2021-04-27 14:12:06 +0300

    v-2.37

--- src/components/filterDate/filterDateList.jsx
+++ src/components/filterDate/filterDateList.jsx
@@ -88,31 +88,57 @@ export const FilterDateList = (props) => {
   const handleChangeYear = (e) => {
     let [...arrYear] = chosenYears;
     let [...arrMpnths] = chosenMonths;
+    let [...arrDays] = chosenDays;
     let Year = Number(e.currentTarget.name);
     let Months = getMonthsInYear(Year);
     if (e.currentTarget.checked) {
       arrYear.push(Year);
       Months.forEach((elem) => {
         arrMpnths.push(`${Year}-${elem}`);
+        props.arrlist.forEach((elem) => {
+          let date = new Date(elem.date);
+          if (date.getFullYear() == Year) {
+            if (!arrDays.includes(date)) {
+              arrDays.push(
+                `${date.getFullYear()}-${date.getMonth()}-${date.getDate()}`
+              );
+            }
+          }
+        });
       });
     } else {
       arrYear = chosenYears.filter((elem) => elem != Year);
       arrMpnths = chosenMonths.filter((elem) => elem.indexOf(`${Year}`));
+      arrDays = chosenDays.filter((elem) => elem.indexOf(`${Year}`));
     }
     setChosenYears(arrYear);
     setChosenMonths(arrMpnths);
+    setChosenDays(arrDays);
     e.currentTarget.checked = !e.currentTarget.checked;
   };
 
   const handleChangeMonth = (e) => {
-    let [...arr] = chosenMonths;
+    let [...arrMonths] = chosenMonths;
+    let [...arrDays] = chosenDays;
     let Month = e.currentTarget.name;
     if (e.currentTarget.checked) {
-      arr.push(Month);
+      arrMonths.push(Month);
+      props.arrlist.forEach((elem) => {
+        let date = new Date(elem.date);
+        if (`${date.getFullYear()}-${date.getMonth()}` == Month) {
+          if (!arrDays.includes(date)) {
+            arrDays.push(
+              `${date.getFullYear()}-${date.getMonth()}-${date.getDate()}`
+            );
+          }
+        }
+      });
     } else {
-      arr = chosenMonths.filter((elem) => elem != Month);
+      arrMonths = chosenMonths.filter((elem) => elem != Month);
+      arrDays = chosenDays.filter((elem) => elem.indexOf(`${Month}`));
     }
-    setChosenMonths(arr);
+    setChosenMonths(arrMonths);
+    setChosenDays(arrDays);
     e.currentTarget.checked = !e.currentTarget.checked;
   };
 
@@ -147,101 +173,111 @@ export const FilterDateList = (props) => {
 
   return (
     <div className="filterDateDiv">
-      {years.map((elemYear) => {
-        let months = getMonthsInYear(elemYear);
-        return (
-          <div key={`Year${elemYear}`} name={elemYear} className="filterStrDiv">
-            <div className="filterBtnP">
-              <button
-                onClick={handlePlusYearClick}
-                name={elemYear}
-                className="filterBtnPlus"
-              >
-                +
-              </button>
-              <p className="filterP">
-                <input
-                  type="checkbox"
-                  onChange={handleChangeYear}
+      <div className="filterYearsDiv">
+        {years.map((elemYear) => {
+          let months = getMonthsInYear(elemYear);
+          return (
+            <div
+              key={`Year${elemYear}`}
+              name={elemYear}
+              className="filterStrDiv"
+            >
+              <div className="filterBtnP">
+                <button
+                  onClick={handlePlusYearClick}
                   name={elemYear}
-                  checked={chosenYears.includes(elemYear)}
-                />
-                {elemYear}
-              </p>
-            </div>
-            {shownYears.includes(elemYear) && (
-              <div className="felterMonthDiv">
-                {months.map((elemMonth) => {
-                  let days = [];
-                  props.arrlist.forEach((elem) => {
-                    let date = new Date(elem.date);
-                    if (
-                      date.getFullYear() == elemYear &&
-                      date.getMonth() == elemMonth
-                    ) {
-                      let day = date.getDate();
-                      if (!days.includes(day)) {
-                        days.push(day);
-                      }
-                    }
-                  });
-                  return (
-                    <div
-                      key={`${elemYear}${elemMonth}`}
-                      className="felterStrDiv"
-                    >
-                      <div className="filterBtnP filterBtnMonth">
-                        <button
-                          onClick={handlePlusMonthClick}
-                          name={`${elemYear}-${elemMonth}`}
-                          className="filterBtnPlus"
-                        >
-                          +
-                        </button>
-                        <p className="filterP">
-                          <input
-                            type="checkbox"
-                            checked={chosenMonths.includes(
-                              `${elemYear}-${elemMonth}`
-                            )}
-                            name={`${elemYear}-${elemMonth}`}
-                            onChange={handleChangeMonth}
-                          />
-                          {nameOfMonth(elemMonth)}
-                        </p>
-                      </div>
-                      {shownMonths.includes(`${elemYear}-${elemMonth}`) && (
-                        <div className="filterDaysDiv">
-                          {days.map((elemDay) => {
-                            return (
-                              <div
-                                key={`${elemYear}-${elemMonth}-${elemDay}`}
-                                className="filterBtnDay"
-                              >
-                                <p className="filterP">
-                                  <input
-                                    type="checkbox"
-                                    checked={chosenDays.includes(
-                                      `${elemYear}-${elemMonth}-${elemDay}`
-                                    )}
-                                    name={`${elemYear}-${elemMonth}-${elemDay}`}
-                                    onChange={handleChangeDay}
-                                  />
-                                  {elemDay}
-                                </p>
-                              </div>
-                            );
-                          })}
-                        </div>
-                      )}
-                    </div>
-                  );
-                })}
+                  className="filterBtnPlus"
+                >
+                  +
+                </button>
+                <p className="filterP">
+                  <input
+                    type="checkbox"
+                    onChange={handleChangeYear}
+                    name={elemYear}
+                    checked={chosenYears.includes(elemYear)}
+                  />
+                  {elemYear}
+                </p>
               </div>
-            )}
-          </div>
-        );
-      })}
+              {shownYears.includes(elemYear) && (
+                <div className="felterMonthDiv">
+                  {months.map((elemMonth) => {
+                    let days = [];
+                    props.arrlist.forEach((elem) => {
+                      let date = new Date(elem.date);
+                      if (
+                        date.getFullYear() == elemYear &&
+                        date.getMonth() == elemMonth
+                      ) {
+                        let day = date.getDate();
+                        if (!days.includes(day)) {
+                          days.push(day);
+                        }
+                      }
+                    });
+                    return (
+                      <div
+                        key={`${elemYear}${elemMonth}`}
+                        className="felterStrDiv"
+                      >
+                        <div className="filterBtnP filterBtnMonth">
+                          <button
+                            onClick={handlePlusMonthClick}
+                            name={`${elemYear}-${elemMonth}`}
+                            className="filterBtnPlus"
+                          >
+                            +
+                          </button>
+                          <p className="filterP">
+                            <input
+                              type="checkbox"
+                              checked={chosenMonths.includes(
+                                `${elemYear}-${elemMonth}`
+                              )}
+                              name={`${elemYear}-${elemMonth}`}
+                              onChange={handleChangeMonth}
+                            />
+                            {nameOfMonth(elemMonth)}
+                          </p>
+                        </div>
+                        {shownMonths.includes(`${elemYear}-${elemMonth}`) && (
+                          <div className="filterDaysDiv">
+                            {days.map((elemDay) => {
+                              return (
+                                <div
+                                  key={`${elemYear}-${elemMonth}-${elemDay}`}
+                                  className="filterBtnDay"
+                                >
+                                  <p className="filterP">
+                                    <input
+                                      type="checkbox"
+                                      checked={chosenDays.includes(
+                                        `${elemYear}-${elemMonth}-${elemDay}`
+                                      )}
+                                      name={`${elemYear}-${elemMonth}-${elemDay}`}
+                                      onChange={handleChangeDay}
+                                    />
+                                    {elemDay}
+                                  </p>
+                                </div>
+                              );
+                            })}
+                          </div>
+                        )}
+                      </div>
+                    );
+                  })}
+                </div>
+              )}
+            </div>
+          );
+        })}
+      </div>
+      <div className="filterBtnOkClear">
+        <button>Ok</button>
+        <button>Clear</button>
+      </div>
     </div>
   );
 };
