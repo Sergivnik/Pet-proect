76c54bd8be8998bae97bfcfdffa2612ea860512a
Serg <sergivnik@mail.ru>
2021-05-12 18:51:30 +0300

    v-1.2

--- src/components/someComponent/someComonent.jsx
+++ src/components/someComponent/someComonent.jsx
@@ -1,12 +1,12 @@
-import React, { useRef } from "react";
+import React from "react";
 
 export const SomeComponent = () => {
-  const vidRef = useRef(null);
   const handleLoadedmetadata = async (e) => {
     console.log(e.target.duration);
     let video = e.target;
     video.volume = 0;
     let div = document.getElementById("canvasDiv");
+    console.log(video.width, video.height);
     let canvas;
     for (let i = 0; i <= video.duration; i = i + video.duration / 5) {
       canvas = document.createElement("canvas");
@@ -16,13 +16,12 @@ export const SomeComponent = () => {
       let ctx = canvas.getContext("2d");
       video.currentTime = i;
       await video.play();
-      ctx.drawImage(video, 0, 0);
+      ctx.drawImage(video, 0, 0, 350, 250);
     }
   };
   return (
     <div>
       <video
-        ref={vidRef}
         //src="https://d2xzmw6cctk25h.cloudfront.net/record/113845/attachment/65ae3d0987c9d31d1d8286e1bf526b37.mp4"
         src="http://techslides.com/demos/sample-videos/small.mp4"
         controls={true}
