a9b43bbc5533a1a0c72e2ce7349899c472989e0d
Sergivnik <65278578+Sergivnik@users.noreply.github.com>
2021-11-10 19:22:49 +0300

    v-2.186

--- src/components/editData/driverTable.jsx
+++ src/components/editData/driverTable.jsx
@@ -2,17 +2,19 @@ import React, { useEffect, useState } from "react";
 import "./editData.sass";
 
 export const DriverTable = (props) => {
-  return <table>
-     <thead>
-        <tr>
-           <td>Краткое название</td>
-           <td>Телефон</td>
-           <td>Полное название</td>
-           <td>ИНН</td>
-           <td>Адрес</td>
-           <td>Расчетный счет</td>
-           <td>Договор</td>
+  return (
+    <table className="driverTbl">
+      <thead className="driverTblTr">
+        <tr className="driverTblTr">
+          <td>Краткое название</td>
+          <td>Телефон</td>
+          <td>Полное название</td>
+          <td>ИНН</td>
+          <td>Адрес</td>
+          <td>Расчетный счет</td>
+          <td>Договор</td>
         </tr>
-     </thead>
-  </table>;
+      </thead>
+    </table>
+  );
 };
