a2774f3b767c7552be96d2086f68ec5db08eddb8
Sergivnik <65278578+Sergivnik@users.noreply.github.com>
2021-11-12 12:01:14 +0300

    dtriverTable-TR-1

--- src/components/editData/driverTableTR.jsx
+++ src/components/editData/driverTableTR.jsx
@@ -5,107 +5,128 @@ import "./editData.sass";
 export const DriverTableTR = (props) => {
   let elem = props.elem;
   const [colNumber, setColNumber] = useState(null);
-  const [widthTd, setWidthTd] = useState(null);
+  const [currentElement, setCurrentElement] = useState(null);
 
   const handleDBLclick = (e) => {
     setColNumber(e.currentTarget.cellIndex);
     props.getCurrentId(elem._id);
     console.log(e.currentTarget.offsetWidth);
-    e.currentTarget.width = e.currentTarget.offsetWidth-2 + "px";
+    e.currentTarget.width = e.currentTarget.offsetWidth - 2 + "px";
+    setCurrentElement(e.currentTarget);
+  };
+  const handleEnter = (e) => {
+    if (e.key == "Enter") {
+      let { ...obj } = elem;
+      switch (colNumber) {
+        case 0:
+          obj.value = e.currentTarget.value;
+          break;
+        case 1:
+          obj.phone = e.currentTarget.value;
+          break;
+        case 2:
+          obj.compfnyName = e.currentTarget.value;
+          break;
+        case 3:
+          obj.TIN = e.currentTarget.value;
+          break;
+        case 4:
+          obj.address = e.currentTarget.value;
+          break;
+        case 5:
+          obj.currentAccount = e.currentTarget.value;
+          break;
+        case 6:
+          obj.contract = e.currentTarget.value;
+          break;
+        default:
+          break;
+      }
+      console.log(obj);
+    }
   };
 
   useEffect(() => {
     if (props.currentId != elem._id) setColNumber(null);
   }, [props.currentId]);
+  useEffect(() => {
+    if (currentElement) currentElement.firstChild.focus();
+  }, [currentElement]);
   return (
     <tr key={"driver" + elem._id}>
-      <td
-        className="driverTd"
-        onDoubleClick={handleDBLclick}
-        onMouseDown={(e) => {
-          if (e.currentTarget.tagName == "TD") e.preventDefault();
-        }}
-      >
+      <td className="driverTd" onDoubleClick={handleDBLclick}>
         {colNumber == 0 ? (
-          <input type="text" className="driverTrInput" />
+          <input
+            type="text"
+            className="driverTrInput"
+            onKeyDown={handleEnter}
+          />
         ) : (
           elem.value
         )}
       </td>
-      <td
-        className="driverTd"
-        onDoubleClick={handleDBLclick}
-        onMouseDown={(e) => {
-          if (e.currentTarget.tagName == "TD") e.preventDefault();
-        }}
-      >
+      <td className="driverTd" onDoubleClick={handleDBLclick}>
         {colNumber == 1 ? (
-          <input type="text" className="driverTrInput" />
+          <input
+            type="text"
+            className="driverTrInput"
+            onKeyDown={handleEnter}
+          />
         ) : (
           elem.phone
         )}
       </td>
-      <td
-        className="driverTd"
-        onDoubleClick={handleDBLclick}
-        onMouseDown={(e) => {
-          if (e.currentTarget.tagName == "TD") e.preventDefault();
-        }}
-      >
+      <td className="driverTd" onDoubleClick={handleDBLclick}>
         {colNumber == 2 ? (
-          <input type="text" className="driverTrInput" />
+          <input
+            type="text"
+            className="driverTrInput"
+            onKeyDown={handleEnter}
+          />
         ) : (
           elem.compfnyName
         )}
       </td>
-      <td
-        className="driverTd"
-        onDoubleClick={handleDBLclick}
-        onMouseDown={(e) => {
-          if (e.currentTarget.tagName == "TD") e.preventDefault();
-        }}
-      >
+      <td className="driverTd" onDoubleClick={handleDBLclick}>
         {colNumber == 3 ? (
-          <input type="text" className="driverTrInput" />
+          <input
+            type="text"
+            className="driverTrInput"
+            onKeyDown={handleEnter}
+          />
         ) : (
           elem.TIN
         )}
       </td>
-      <td
-        className="driverTd"
-        onDoubleClick={handleDBLclick}
-        onMouseDown={(e) => {
-          if (e.currentTarget.tagName == "TD") e.preventDefault();
-        }}
-      >
+      <td className="driverTd" onDoubleClick={handleDBLclick}>
         {colNumber == 4 ? (
-          <input type="text" className="driverTrInput" />
+          <input
+            type="text"
+            className="driverTrInput"
+            onKeyDown={handleEnter}
+          />
         ) : (
           elem.address
         )}
       </td>
-      <td
-        className="driverTd"
-        onDoubleClick={handleDBLclick}
-        onMouseDown={(e) => {
-          if (e.currentTarget.tagName == "TD") e.preventDefault();
-        }}
-      >
+      <td className="driverTd" onDoubleClick={handleDBLclick}>
         {colNumber == 5 ? (
-          <input type="text" className="driverTrInput" />
+          <input
+            type="text"
+            className="driverTrInput"
+            onKeyDown={handleEnter}
+          />
         ) : (
           elem.currentAccount
         )}
       </td>
-      <td
-        className="driverTd"
-        onDoubleClick={handleDBLclick}
-        onMouseDown={(e) => {
-          if (e.currentTarget.tagName == "TD") e.preventDefault();
-        }}
-      >
+      <td className="driverTd" onDoubleClick={handleDBLclick}>
         {colNumber == 6 ? (
-          <input type="text" className="driverTrInput" />
+          <input
+            type="text"
+            className="driverTrInput"
+            onKeyDown={handleEnter}
+          />
         ) : (
           elem.contract
         )}
