91274b85bcfce964d98ec8c537d5e1193c116d92
Sergivnik <65278578+Sergivnik@users.noreply.github.com>
2021-12-01 19:29:24 +0300

    try do some-2

--- src/actions/documentAction.js
+++ src/actions/documentAction.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { URL } from "../middlewares/initialState";
+import { DOMENNAME } from "../middlewares/initialState";
 
 export const GET_PDF_SUCCESS = "GET_PDF_SUCCESS";
 export const GET_PDF_FAILURE = "GET_PDF_FAILURE";
@@ -14,12 +14,12 @@ export const getPdfFailure = () => ({
 export const getPdf = (id) => {
     return (dispatch) => {
         axios
-            .post(URL + "/getPdf", { responseType: 'arraybuffer', body: id })
+            .post(DOMENNAME + "/API/getPdf", { responseType: 'blob', body: id })
             .then((res) => {
-                let link = document.createElement('a');
-                let blob = new Blob([res.data],{type: "application/pdf"});
-                link.href = URL.createObjectURL(blob);
-                link.click();
+                const url = window.URL.createObjectURL(new Blob([res.data],{type: "application/pdf"}));
+                //let blob = new Blob([res.data],{type: "application/pdf"});
+                //let href = URL.createObjectURL(blob);
+                window.open(url);
             })
             .catch((e) => {
                 console.log(e.message);
