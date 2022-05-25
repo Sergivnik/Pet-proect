004dc33598ce495034e64bf330a9aa4e4d1be7d7
Sergivnik <65278578+Sergivnik@users.noreply.github.com>
2022-05-25 13:38:50 +0300

    specialTable - 10

--- src/components/specialTable/specialTable.jsx
+++ src/components/specialTable/specialTable.jsx
@@ -6,20 +6,46 @@ import "./specialTable.sass";
 import { SpecialTableHeaderTr } from "./specilTableHeaderTr.jsx";
 
 export const SpecialTable = () => {
-  const tableData = useSelector((state) => state.oderReducer.addtable);
+  const addTable = useSelector((state) => state.oderReducer.addtable);
+  const ordersList = useSelector((state) => state.oderReducer.odersList);
 
   const [currentId, setCurrentId] = useState(null);
+  const [tableData, setTableData] = useState([]);
+  const [filterData, setFilterData] = useState({});
 
   const getCurrentId = (id) => {
     console.log(id);
     setCurrentId(id);
   };
+  useEffect(() => {
+    let [...arr] = addTable;
+    arr.sort((a, b) => {
+      let aOrder = ordersList.find((elem) => elem._id == a.orderId);
+      let bOrder = ordersList.find((elem) => elem._id == b.orderId);
+      if (aOrder.accountNumber > bOrder.accountNumber) return 1;
+      if (aOrder.accountNumber == bOrder.accountNumber) return 0;
+      if (aOrder.accountNumber < bOrder.accountNumber) return -1;
+    });
+    setTableData(arr);
+    let ordersIdList = [];
+    arr.forEach((elem) => {
+      if (ordersIdList.indexOf(elem.customerId) === -1)
+        ordersIdList.push(elem.customerId);
+    });
+    setFilterData({
+      customerId: ordersIdList,
+      safe: null,
+      card: null,
+      customerPayment: null,
+      returnPayment: null,
+    });
+  }, [addTable]);
 
   return (
     <div className="specialTableMainDiv">
       <table className="specialTableMainTable">
         <thead className="specialTableThead">
-          <SpecialTableHeaderTr />
+          <SpecialTableHeaderTr filterData={filterData} />
         </thead>
         <tbody className="specialTableBody">
           {tableData.map((elem) => {
--- src/components/specialTable/specilTableHeaderTr.jsx
+++ src/components/specialTable/specilTableHeaderTr.jsx
@@ -1,7 +1,8 @@
 import React, { useEffect, useState } from "react";
 import "./specialTable.sass";
 
-export const SpecialTableHeaderTr = () => {
+export const SpecialTableHeaderTr = (props) => {
+  console.log(props.filterData);
   return (
     <tr>
       <td className="specialTableHeaderTd">Клиент</td>
