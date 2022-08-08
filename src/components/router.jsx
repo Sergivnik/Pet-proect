0d90f71fd032122482e6753093b2397cb0649769
Sergivnik <65278578+Sergivnik@users.noreply.github.com>
2022-08-08 10:24:08 +0300

    Revert "test - 1"
    This reverts commit ab86cd8889a44e5dc00881c85e9cbaf67eab4169.

--- src/components/router.jsx
+++ src/components/router.jsx
@@ -1,15 +1,15 @@
 import React from "react";
 import { Switch, Route } from "react-router-dom";
 import { App } from "../App.jsx";
-import { SomeComponent } from "./someComponent/someComonent.jsx";
-import { Oders } from "./oders/oders.jsx";
+import {SomeComponent} from "./someComponent/someComonent.jsx"
+import {Oders} from "./oders/oders.jsx"
 
 export const Router = () => {
   return (
     <Switch>
+      <Route exact path="/" component={App}></Route>
       <Route exact path="/something" component={SomeComponent}></Route>
       <Route exact path="/oders" component={Oders}></Route>
-      <Route exact path="/" component={App}></Route>
     </Switch>
   );
 };
