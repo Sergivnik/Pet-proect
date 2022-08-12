7178c21a99abb6d7ef187681f1068d1fcc4ae794
Sergivnik <65278578+Sergivnik@users.noreply.github.com>
2022-08-12 13:26:40 +0300

    auth-3

--- src/components/auth/auth.jsx
+++ src/components/auth/auth.jsx
@@ -1,9 +1,38 @@
 import React from "react";
+import "./auth.sass";
 
 export const Auth = () => {
+  const handleClickSignIn = (e) => {
+    let div = e.currentTarget;
+    let siblings = div.parentNode.childNodes;
+    siblings.forEach((node) => (node.style.borderBottom = "1px solid #000"));
+    div.style.borderBottom = "none";
+  };
   return (
-    <div>
-      <h3>Пароль</h3>
+    <div className="authContainer">
+      <div className="authWrap">
+        <header className="authHeader">
+          <div className="authHeaderSignIn" onClick={handleClickSignIn}>
+            Войти
+          </div>
+          <div className="authHeaderSignUp" onClick={handleClickSignIn}>
+            Зарегистрироваться
+          </div>
+        </header>
+        <main className="authMain">
+          <label className="authLabel">
+            <span className="authSpan">Логин</span>
+            <input className="authInput" />
+          </label>
+          <label className="authLabel">
+            <span className="authSpan">Пароль</span>
+            <input className="authInput" />
+          </label>
+        </main>
+        <footer className="authFooter">
+          <button>Вход</button>
+        </footer>
+      </div>
     </div>
   );
 };
