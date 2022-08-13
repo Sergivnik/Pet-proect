6cc08f280b508475e6e8df909d78a4b7053a689b
Sergivnik <65278578+Sergivnik@users.noreply.github.com>
2022-08-13 11:57:10 +0300

    auth-4

---
+++ src/actions/auth.js
@@ -0,0 +1,5 @@
+import axios from "axios";
+import { URL } from "../middlewares/initialState";
+
+export const AUTH_SIGN_UP_SUCCESS = "AUTH_SIGN_UP_SUCCESS";
+export const AUTH_SIGN_UP_FAILURE = "AUTH_SIGN_UP_FAILURE";
--- src/components/auth/auth.jsx
+++ src/components/auth/auth.jsx
@@ -1,12 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
 import "./auth.sass";
 
 export const Auth = () => {
+  const [btnName, setBtnName] = useState("Вход");
+  const [login, setLogin] = useState("");
+  const [password, setPassword] = useState("");
+
   const handleClickSignIn = (e) => {
     let div = e.currentTarget;
     let siblings = div.parentNode.childNodes;
-    siblings.forEach((node) => (node.style.borderBottom = "1px solid #000"));
+    siblings.forEach((node) => {
+      node.style.borderBottom = "1px solid #000";
+      node.style.backgroundColor = "#FFFFFF";
+    });
     div.style.borderBottom = "none";
+    div.style.backgroundColor = "#cdcdcd";
+    setBtnName("Войти");
+  };
+  const handleClickSignUp = (e) => {
+    let div = e.currentTarget;
+    let siblings = div.parentNode.childNodes;
+    siblings.forEach((node) => {
+      node.style.borderBottom = "1px solid #000";
+      node.style.backgroundColor = "#FFFFFF";
+    });
+    div.style.borderBottom = "none";
+    div.style.backgroundColor = "#cdcdcd";
+    setBtnName("Зарегистрироваться");
+  };
+  const handleChangeLogin = (e) => {
+    setLogin(e.currentTarget.value);
+  };
+  const handleChangePassword = (e) => {
+    setPassword(e.currentTarget.value);
+  };
+  const handleClickBtn = () => {
+    console.log("login ", login, " password ", password);
   };
   return (
     <div className="authContainer">
@@ -15,22 +44,32 @@ export const Auth = () => {
           <div className="authHeaderSignIn" onClick={handleClickSignIn}>
             Войти
           </div>
-          <div className="authHeaderSignUp" onClick={handleClickSignIn}>
+          <div className="authHeaderSignUp" onClick={handleClickSignUp}>
             Зарегистрироваться
           </div>
         </header>
         <main className="authMain">
           <label className="authLabel">
             <span className="authSpan">Логин</span>
-            <input className="authInput" />
+            <input
+              className="authInput"
+              type="text"
+              value={login}
+              onChange={handleChangeLogin}
+            />
           </label>
           <label className="authLabel">
             <span className="authSpan">Пароль</span>
-            <input className="authInput" />
+            <input
+              className="authInput"
+              type="password"
+              value={password}
+              onChange={handleChangePassword}
+            />
           </label>
         </main>
         <footer className="authFooter">
-          <button>Вход</button>
+          <button onClick={handleClickBtn}>{btnName}</button>
         </footer>
       </div>
     </div>
