97587c3cdddfbc7f0fbf2ad03c879cb572bddabf
Sergivnik <65278578+Sergivnik@users.noreply.github.com>
2022-11-21 19:17:35 +0300

    Customer part - 1.13

--- src/components/customerPart/customerOrders/customerCreateApp.jsx
+++ src/components/customerPart/customerOrders/customerCreateApp.jsx
@@ -46,6 +46,59 @@ export const CustomerCreateApp = (props) => {
   }, [props.id]);
   const getData = (data, name) => {
     console.log(data, name);
+    let obj = { ...dataApp };
+    obj.dateOfApp = data;
+    setDataApp(obj);
+  };
+
+  const addPointData = (pointData, name) => {
+    console.log(pointData);
+    let obj = { ...dataApp };
+    if (name == "loadingPoint") {
+      obj.idLoadingPoint.push(pointData.idPoint);
+      obj.loadingInfo.push(pointData.text);
+      obj.dateOfLoading.push(pointData.date);
+      obj.loadingStoreId.push(pointData.storeId);
+    }
+    if (name == "unloadingPoint") {
+      obj.idUnloadingPoint.push(pointData.idPoint);
+      obj.unloadingInfo.push(pointData.text);
+      obj.dateOfUnloading.push(pointData.date);
+      obj.unloadingStoreId.push(pointData.storeId);
+    }
+    setDataApp(obj);
+  };
+  const editData = (name, field, data, index) => {
+    let obj = { ...dataApp };
+    if (name == "loadingPoint") {
+      if (field == "date") {
+        obj.dateOfLoading[index] = data;
+      }
+      if (field == "point") {
+        obj.idLoadingPoint[index] = data;
+      }
+      if (field == "store") {
+        obj.loadingStoreId[index] = data;
+      }
+      if (field == "text") {
+        obj.loadingInfo[index] = data;
+      }
+    }
+    if (name == "unloadingPoint") {
+      if (field == "date") {
+        obj.dateOfUnloading[index] = data;
+      }
+      if (field == "point") {
+        obj.idUnloadingPoint[index] = data;
+      }
+      if (field == "store") {
+        obj.unloadingStoreId[index] = data;
+      }
+      if (field == "text") {
+        obj.unloadingInfo[index] = data;
+      }
+    }
+    setDataApp(obj);
   };
   return (
     <div className="contentDiv">
@@ -67,6 +120,8 @@ export const CustomerCreateApp = (props) => {
           pointList={dataApp.idLoadingPoint}
           storeList={dataApp.loadingStoreId}
           infoList={dataApp.loadingInfo}
+          addPointData={addPointData}
+          editData={editData}
         />
       </div>
     </div>
--- src/components/customerPart/customerOrders/customerPointForm.jsx
+++ src/components/customerPart/customerOrders/customerPointForm.jsx
@@ -8,13 +8,102 @@ export const CustomerPointForm = (props) => {
   const storelist = useSelector((state) => state.customerReducer.storelist);
 
   const [showAddPoint, setShowAddPoint] = useState(true);
+  const [pointData, setPointData] = useState({});
+  const [storelistInput, setStoreListInput] = useState(storelist);
+  const [selectedIndex, setSelectedIndex] = useState(null);
+  const [editDate, setEditDate] = useState(false);
+  const [editPoint, setEditPoint] = useState(false);
+  const [editStore, setEditStore] = useState(false);
+  const [editText, setEditText] = useState(false);
 
   useEffect(() => {
     if (props.pointList.length != 0) setShowAddPoint(false);
   }, [props]);
+  useEffect(() => {
+    if (showAddPoint) {
+      let inputDate = document.querySelector(".customerPointInput");
+      inputDate.focus();
+    }
+  }, [showAddPoint]);
 
+  const handleDateBlur = (e) => {
+    let obj = { ...pointData };
+    obj.date = e.currentTarget.value;
+    if (e.currentTarget.value) {
+      setPointData(obj);
+      let inputCity = document.querySelector("#point");
+      inputCity.focus();
+    } else {
+      e.currentTarget.focus();
+    }
+  };
+  const handleDateEnter = (e) => {
+    if (e.key == "Enter") {
+      let obj = { ...pointData };
+      obj.date = e.currentTarget.value;
+      if (e.currentTarget.value) {
+        setPointData(obj);
+        let inputCity = document.querySelector("#point");
+        inputCity.focus();
+      } else {
+        e.currentTarget.focus();
+      }
+    }
+  };
   const setPoint = (data) => {
+    let obj = { ...pointData };
+    obj.idPoint = data._id;
+    setPointData(obj);
+    setStoreListInput(storelist.filter((store) => store.idCity == data._id));
+    let inputStore = document.querySelector("#store");
+    inputStore.focus();
+  };
+  const handlePointBlur = (e) => {
+    if (pointData.idPoint) {
+      let inputStore = document.querySelector("#store");
+      e.currentTarget.style.borderColor = "black";
+      inputStore.focus();
+    } else {
+      e.currentTarget.style.borderColor = "red";
+    }
+  };
+  const setStore = (data) => {
+    let obj = { ...pointData };
+    obj.storeId = data._id;
+    setPointData(obj);
+    let inputTextInfo = document.querySelector("#textInfo");
+    inputTextInfo.focus();
+  };
+  const handleBlurText = (e) => {
+    let obj = { ...pointData };
+    obj.text = e.currentTarget.value;
+    setPointData(obj);
+    props.addPointData(obj, props.name);
+  };
+  const handleDblClick = (name, index) => {
+    if (name == "date") setEditDate(true);
+    if (name == "point") setEditPoint(true);
+    if (name == "store") setEditStore(true);
+    if (name == "text") setEditText(true);
+    setSelectedIndex(index);
+  };
+  const handleDateEditBlur = (e) => {
+    props.editData(props.name, "date", e.currentTarget.value, selectedIndex);
+    setEditDate(false);
+  };
+  const handleEditPoint = (data) => {
+    props.editData(props.name, "point", data._id, selectedIndex);
+    setEditPoint(false);
+    setStoreListInput(storelist.filter((store) => store.idCity == data._id));
+  };
+  const handleEditStore = (data) => {
     console.log(data);
+    props.editData(props.name, "store", data._id, selectedIndex);
+    setEditStore(false);
+  };
+  const handleEditText = (e) => {
+    props.editData(props.name, "text", e.currentTarget.value, selectedIndex);
+    setEditText(false);
   };
   return (
     <div className="customerPointFormDiv">
@@ -26,25 +115,101 @@ export const CustomerPointForm = (props) => {
       </header>
       <main className="customerPointFormMain">
         {props.pointList.map((pointId, index) => {
-          <div key={`pointData-${index}`} className="customerPointDataDiv">
-            <div
-              onClick={() => {
-                handleClickMinis(index);
-              }}
-            >
-              <svg viewBox="0 0 120 120" className="PFBtnSvg">
-                <circle
-                  cx="60"
-                  cy="60"
-                  r="50"
-                  stroke="black"
-                  strokeWidth="10"
-                  fill="none"
-                />
-                <rect x="15" y="50" width="90" height="20" rx="10" ry="10" />
-              </svg>
+          let point = citiesList.find((point) => point._id == pointId);
+          let store = storelist.find(
+            (store) => store._id == props.storeList[index]
+          );
+          return (
+            <div key={`pointData-${index}`} className="customerPointDataDiv">
+              <div
+                className="wrapprDivSVG"
+                onClick={() => {
+                  handleClickMinis(index);
+                }}
+              >
+                <svg viewBox="0 0 120 120" className="PFBtnSvg">
+                  <circle
+                    cx="60"
+                    cy="60"
+                    r="50"
+                    stroke="black"
+                    strokeWidth="10"
+                    fill="none"
+                  />
+                  <rect x="15" y="50" width="90" height="20" rx="10" ry="10" />
+                </svg>
+              </div>
+              {editDate && selectedIndex == index ? (
+                <div className="customerPointInputWrapper">
+                  <input
+                    type="date"
+                    className="customerPointInput"
+                    onBlur={handleDateEditBlur}
+                  />
+                </div>
+              ) : (
+                <span
+                  onDoubleClick={() => {
+                    handleDblClick("date", index);
+                  }}
+                >
+                  {props.dateList[index]}
+                </span>
+              )}
+              {editPoint && selectedIndex == index ? (
+                <div className="customerPointInputWrapper">
+                  <ChoiseList
+                    name="point"
+                    arrlist={citiesList}
+                    setValue={handleEditPoint}
+                  />
+                </div>
+              ) : (
+                <span
+                  onDoubleClick={() => {
+                    handleDblClick("point", index);
+                  }}
+                >
+                  {point ? point.value : null}
+                </span>
+              )}
+              {editStore && selectedIndex == index ? (
+                <div className="customerPointInputWrapper">
+                  <ChoiseList
+                    name="store"
+                    arrlist={storelistInput}
+                    setValue={handleEditStore}
+                  />
+                </div>
+              ) : (
+                <span
+                  onDoubleClick={() => {
+                    handleDblClick("store", index);
+                  }}
+                >
+                  {store ? store.value : null}
+                </span>
+              )}
+              {editText && selectedIndex == index ? (
+                <div className="customerPointInputWrapper">
+                  <input
+                    type="text"
+                    id="textInfo"
+                    className="customerPointInput"
+                    onBlur={handleEditText}
+                  />
+                </div>
+              ) : (
+                <span
+                  onDoubleClick={() => {
+                    handleDblClick("text", index);
+                  }}
+                >
+                  {props.infoList[index]}
+                </span>
+              )}
             </div>
-          </div>;
+          );
         })}
         {showAddPoint ? (
           <div className="customerPointDataDiv">
@@ -62,9 +227,14 @@ export const CustomerPointForm = (props) => {
               </svg>
             </div>
             <div className="customerPointInputWrapper">
-              <input type="date" className="customerPointInput" />
+              <input
+                type="date"
+                className="customerPointInput"
+                onBlur={handleDateBlur}
+                onKeyDown={handleDateEnter}
+              />
             </div>
-            <div className="customerPointInputWrapper">
+            <div className="customerPointInputWrapper" onBlur={handlePointBlur}>
               <ChoiseList
                 name="point"
                 arrlist={citiesList}
@@ -73,13 +243,18 @@ export const CustomerPointForm = (props) => {
             </div>
             <div className="customerPointInputWrapper">
               <ChoiseList
-                name="point"
-                arrlist={storelist}
-                setValue={setPoint}
+                name="store"
+                arrlist={storelistInput}
+                setValue={setStore}
               />
             </div>
             <div className="customerPointInputWrapper">
-              <input type="text" className="customerPointInput" />
+              <input
+                type="text"
+                id="textInfo"
+                className="customerPointInput"
+                onBlur={handleBlurText}
+              />
             </div>
           </div>
         ) : (
