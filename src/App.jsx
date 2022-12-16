b004d726944a31733849dc931918db9bcef6b41c
Sergivnik <65278578+Sergivnik@users.noreply.github.com>
2022-12-16 14:54:44 +0300

    app style

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,28 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
+import "./app.sass";
+import { DOMENNAME } from "./middlewares/initialState";
 
 export const App = () => {
-  const [text, setText] = useState("Стартовая страница");
-
-  setTimeout(() => setText("Обновленный React-компонент"), 1000);
-
   return (
-    <div>
-      {text}
-      <Link to="/something">Something</Link>
-      <Link to="/oders">Заказы</Link>
-      <Link to="/customer">Клиент</Link>
-      <Link to="/auth">Вход</Link>
+    <div className="appRootDiv">
+      <header className="appRootHeader">
+        <div className="appRootLogo">
+          <img
+            className="appRootImg"
+            src={`${DOMENNAME}/img/track3.png`}
+            height="50"
+            width="80"
+          />
+        </div>
+        <div className="appRootMenu">
+          <Link to="/">Home</Link>
+          <Link to="/something">Something</Link>
+          <Link to="/oders">Заказы</Link>
+          <Link to="/customer">Клиент</Link>
+          <Link to="/auth">Вход</Link>
+        </div>
+      </header>
     </div>
   );
 };
