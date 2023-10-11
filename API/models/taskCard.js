c92209402982858b5ddeaa6798dbcab99f48281b
Sergivnik <65278578+Sergivnik@users.noreply.github.com>
2023-10-11 15:45:42 +0300

    mySQL transaction taskCard

--- API/models/taskCard.js
+++ API/models/taskCard.js
@@ -8,9 +8,13 @@ let TascsCard = {
     let sumOfCustomerDebts = 0;
     let driverPaymentId, customerPaymentId;
     const db = mysql.createPool(options.sql).promise();
+    const connection = await db.getConnection();
+
     try {
+      await connection.beginTransaction();
+
       if (data.driverDebtsId.length > 0) {
-        let [listDriverDebts] = await db.query(
+        let [listDriverDebts] = await connection.query(
           `SELECT * FROM driverdebts where id in (${data.driverDebtsId});`
         );
         sumOfDriverDebts = listDriverDebts.reduce(
@@ -19,8 +23,9 @@ let TascsCard = {
         );
         console.log(sumOfDriverDebts);
       }
+
       if (data.customerDebtsId.length > 0) {
-        let [listOfCustomerDebts] = await db.query(
+        let [listOfCustomerDebts] = await connection.query(
           `SELECT sum,interest,customerPrice FROM addtable, oderslist where _id=orderId and id in (${data.customerDebtsId})`
         );
         sumOfCustomerDebts = listOfCustomerDebts.reduce(
@@ -32,6 +37,7 @@ let TascsCard = {
         );
         console.log(sumOfCustomerDebts);
       }
+
       let now = new Date();
       let Year = String(now.getFullYear());
       let Month = String(now.getMonth() + 1);
@@ -46,32 +52,38 @@ let TascsCard = {
         sumOfPayment: sumOfDriverDebts + sumOfCustomerDebts,
         listOfDebts: JSON.stringify(data),
       };
+
       if (data.driverDebtsId.length > 0 || data.customerDebtsId.length > 0) {
         console.log(
           cardPayment.date,
           cardPayment.sumOfPayment,
           "${cardPayment.listOfDebts}"
         );
-        await db.query(`INSERT INTO cardpayment set ?`, cardPayment);
+        await connection.query(`INSERT INTO cardpayment set ?`, cardPayment);
       }
+
       if (data.driverDebtsId.length > 0) {
-        await db.query(
+        await connection.query(
           `UPDATE driverdebts set card=1 where id in (${data.driverDebtsId})`
         );
-        let [dataId] = await db.query(
+        let [dataId] = await connection.query(
           `INSERT contractorspayments(idContractor,date,sum,category) VALUES (5,'${cardPayment.date}',${sumOfDriverDebts},2)`
         );
         driverPaymentId = dataId.insertId;
       }
+
       if (data.customerDebtsId.length > 0) {
-        await db.query(
+        await connection.query(
           `UPDATE addtable set card=1 where id in (${data.customerDebtsId})`
         );
-        let [dataId] = await db.query(
+        let [dataId] = await connection.query(
           `INSERT contractorspayments(idContractor,date,sum,category) VALUES (5,'${cardPayment.date}',${sumOfCustomerDebts},3)`
         );
         customerPaymentId = dataId.insertId;
       }
+
+      await connection.commit();
+
       callBack({
         sumOfDriverDebts: sumOfDriverDebts,
         sumOfCustomerDebts: sumOfCustomerDebts,
@@ -79,9 +91,12 @@ let TascsCard = {
         customerPaymentId: customerPaymentId,
       });
     } catch (err) {
+      await connection.rollback();
       callBack({ error: err });
+    } finally {
+      connection.release();
     }
-    db.end();
+db.end();
   },
 };
 
