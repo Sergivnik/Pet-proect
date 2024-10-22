10c9b17f0cd77cadb3fbdb36afe1ca7845618f48
Sergivnik <65278578+Sergivnik@users.noreply.github.com>
2024-10-22 16:32:06 +0300

    driverPaymentsList - v 1.3

--- src/components/driverComponents/driverPaymentsList.tsx
+++ src/components/driverComponents/driverPaymentsList.tsx
@@ -2,14 +2,54 @@ import React, { useEffect, useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { getDataDriverPayments } from "../../actions/driverActions.js";
 import "./driverForms.sass";
+
+export interface DriverPayment {
+  id: number;
+  date: Date;
+  idDriver: number;
+  sumOfPayment: number;
+  listOfOders: number[];
+  sumOfDebts: number;
+  listOfDebts: DriverDebtInfo[];
+}
+export interface DriverDebt {
+  id: number;
+  date: Date;
+  idDriver: number;
+  category: string;
+  sumOfDebt: number;
+  debtClosed: driverDebtStatus;
+  addInfo: string;
+  paidPartOfDebt: number;
+  card: boolean;
+}
+export interface DriverDebtInfo {
+  id: number;
+  sum: number;
+}
+export type driverDebtStatus = "Ок" | "частично" | "нет";
+
 export const DriverPaymentsList = () => {
+  const driverPaymentList: DriverPayment[] = useSelector(
+    (state: any) => state.driverReducer.driverpayment
+  );
+  const status: string = useSelector(
+    (state: any) => state.driverReducer.status
+  );
   const dispatch = useDispatch();
   useEffect(() => {
     dispatch(getDataDriverPayments());
-    console.log("Hi");
   }, []);
+  useEffect(() => {
+    setStatusRequest(status);
+  }, [status]);
+
+  const [statusRequest, setStatusRequest] = useState<string | null>(null);
   return (
     <div className="driverPaymentsListMainDiv">
+      {statusRequest != null && (
+        <div className="statusRequestDiv">{statusRequest}</div>
+      )}
       <table className="driverPaymentsLitMainTable">
         <thead className="driverPaymentsListMainTableThead">
           <tr>
@@ -21,7 +61,7 @@ export const DriverPaymentsList = () => {
         </thead>
         <tbody>
           <tr>
-            <td>Lorem</td>
+            <td>Lorem ipsum</td>
           </tr>
         </tbody>
       </table>
