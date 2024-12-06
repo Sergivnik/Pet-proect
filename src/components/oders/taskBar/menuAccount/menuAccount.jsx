fa24dd41918ffdcef281f4cf6dd5fd6cebc91291
Sergivnik <65278578+Sergivnik@users.noreply.github.com>
2024-12-06 18:32:51 +0300

    add deposit

--- src/components/oders/taskBar/menuAccount/menuAccount.jsx
+++ src/components/oders/taskBar/menuAccount/menuAccount.jsx
@@ -1,10 +1,38 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import { DOMENNAME } from "../../../../middlewares/initialState";
 import { Forecast } from "../../../forecast/forecast.jsx";
 import "./menuAccount.sass";
+import { useSelector, useDispatch } from "react-redux";
+import { editYearConst } from "../../../../actions/reportActions.js";
 
 export const MenuAccount = (props) => {
+  const dispatch = useDispatch();
+  let yearconst = useSelector((state) => state.oderReducer.yearconst);
+  const [deposit, setDeposit] = useState(null);
+  const [editDeposit, setEditDeposit] = useState(false);
+
+  useEffect(() => {
+    if (yearconst.deposit) setDeposit(yearconst.deposit);
+  }, [yearconst]);
+
+  const handleEditDeposit = () => {
+    setEditDeposit(true);
+  };
+  const handleGetDeposit = (e) => {
+    if (e.currentTarget.name == "deposit") {
+      setDeposit(e.currentTarget.value);
+    }
+  };
+  const handleEnterDeposit = (e) => {
+    if (e.code == "Enter" || e.code == "NumpadEnter") {
+      if (e.currentTarget.name == "deposit") {
+        dispatch(editYearConst("deposit", deposit));
+        setEditDeposit(false);
+      }
+    }
+  };
+
   return (
     <div className="wrapperForAccount">
       <div className="orderLogo">
@@ -20,11 +48,28 @@ export const MenuAccount = (props) => {
       <div className="orderDivInfoSpan">
         <span>Рас.сч. </span>
         <span className="spanNoSpace">
-          {props.sumAccount.toLocaleString()} руб.
+          {(props.sumAccount - Number(deposit)).toLocaleString()} руб.
         </span>
       </div>
       <div className="orderDivInfoSpan">
         <Forecast />
+      </div>
+      <div className="orderDivInfoSpan" onDoubleClick={handleEditDeposit}>
+        <span>Депозит </span>
+        {!editDeposit ? (
+          <span className="spanNoSpace">
+            {deposit ? Number(deposit).toLocaleString() : 0} руб.
+          </span>
+        ) : (
+          <input
+            className="inputDeposit"
+            type="number"
+            value={deposit ? deposit : 0}
+            onChange={handleGetDeposit}
+            onKeyDown={handleEnterDeposit}
+            name="deposit"
+          />
+        )}
       </div>
     </div>
   );
